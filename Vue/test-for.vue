5d14a09d753d4e9cc63531e3d6111e72bb0d3ab9
zhanghao.gino <zhanghao.gino@bytedance.com>
2019-08-16 19:07:00 +0800

    init
    Change-Id: I881d29d254bc583152aedbb6b1e513df643ba4c5

---
+++ Vue/test-for.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <a v-for="item in obj" :key="item"></a>
+        <ul>
+            <li v-for="item in obj" :key="item"></li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
